798f1124984b1707f71a6e48ca8437d25cad0624
Amine Bou <aminecmi@gmail.com>
2018-05-24 22:36:20 +0200

    Added the ability to force push and force tag creation.

--- version.sh
+++ version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# You can pass --force as first parameter to force push and tag creation.
 BASE_VERSION="v1.6"
 TODAYS_VERSION="1"
 
@@ -7,8 +7,8 @@ VERSION="${BASE_VERSION}.$(date '+%y%m%j')$TODAYS_VERSION"
 
 echo "Creating tag $VERSION"
 
-git tag $VERSION
+git tag $VERSION $1
 
 echo "Pushing tag"
 
-git push origin $VERSION
+git push origin $VERSION $1
