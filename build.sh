e11dee220f53cd49cf8922b47c261ae2894b33c0
Amine Bou <aminecmi@gmail.com>
2018-05-25 22:22:32 +0200

    Adde dHTTP version check for fdroid.

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+BASE_VERSION="v1.6"
+TODAYS_VERSION="1"
+
+VERSION="${BASE_VERSION}.$(date '+%y%m%j')$TODAYS_VERSION"
+
+./version.sh ${VERSION} $@
+
+./publish-version.sh ${VERSION} $@
--- version.sh
+++ version.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 # You can pass --force as first parameter to force push and tag creation.
-BASE_VERSION="v1.6"
-TODAYS_VERSION="1"
 
-VERSION="${BASE_VERSION}.$(date '+%y%m%j')$TODAYS_VERSION"
+echo "Creating tag $1"
 
-echo "Creating tag $VERSION"
-
-git tag $VERSION $1
+git tag $1 $@
 
 echo "Pushing tag"
 
-git push origin $VERSION $1
+git push origin $1 $@
