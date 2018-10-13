598149d4cd8e27a67b69e289dd79cba597949279
Amine <amine@alantaya.com>
2018-10-13 22:13:56 +0200

    Publish version.

---
+++ publish-version.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -f version.txt
+printf "versionName=$1-github\nversionCode=$1" >> version.txt
+
+# You'll need to change server as your server and define a VERSION_PATH.
+scp version.txt server:$VERSION_PATH
+
+rm version.txt
