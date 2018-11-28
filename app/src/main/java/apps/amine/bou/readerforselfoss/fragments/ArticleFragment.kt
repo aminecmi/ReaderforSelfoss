0b07f2a407302e5aaffe7ca3c510bb8677d70ed1
Amine <amine@alantaya.com>
2018-11-28 21:03:00 +0100

    Closes #174. Closes #248.

--- app/src/main/java/apps/amine/bou/readerforselfoss/fragments/ArticleFragment.kt
+++ app/src/main/java/apps/amine/bou/readerforselfoss/fragments/ArticleFragment.kt
@@ -412,6 +412,7 @@ class ArticleFragment : Fragment() {
             baseUrl,
             """<html>
                 |<head>
+                |   <meta name="viewport" content="width=device-width, initial-scale=1">
                 |   <style>
                 |      img {
                 |        display: inline-block;
