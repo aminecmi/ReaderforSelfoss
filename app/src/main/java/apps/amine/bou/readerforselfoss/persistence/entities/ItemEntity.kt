36a43b3861f0cc98b473813118e81b486c9321fd
davidoskky <davidoskky@yahoo.it>
2020-12-07 15:48:58 +0100

    Allow the value of the thumbnail to be null. (#311)
    Closes #310

--- app/src/main/java/apps/amine/bou/readerforselfoss/persistence/entities/ItemEntity.kt
+++ app/src/main/java/apps/amine/bou/readerforselfoss/persistence/entities/ItemEntity.kt
@@ -20,7 +20,7 @@ data class ItemEntity(
     @ColumnInfo(name = "starred")
     var starred: Boolean,
     @ColumnInfo(name = "thumbnail")
-    val thumbnail: String,
+    val thumbnail: String?,
     @ColumnInfo(name = "icon")
     val icon: String,
     @ColumnInfo(name = "link")
